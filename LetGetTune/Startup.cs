04e59b77264ff3d050ce9cb3637e9a8746de3a47
sangviehuy <hvsang2101@gmail.com>
2018-08-27 20:35:09 +0700

    Add project files.

---
+++ LetGetTune/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetGetTune.Startup))]
+namespace LetGetTune
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
